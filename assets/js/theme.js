44e98cca098600616ae69fdd1507b5c50c1b6694
corporativeaccount <scalambas001@gmail.com>
2025-11-13 10:19:06 -0500

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -17,3 +17,13 @@
     });
   }
 })();
+
+// --- Menú móvil ---
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+if (menuToggle && navMenu) {
+  menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+  });
+}
